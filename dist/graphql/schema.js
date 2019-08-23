92e967ebb2cc82b7c50a006011e8debdde1f07b4
DavidLuna0 <ddavidr569@gmail.com>
2019-08-23 07:17:27 -0300

    Atualizando user schema

--- dist/graphql/schema.js
+++ dist/graphql/schema.js
@@ -1,56 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const graphql_tools_1 = require("graphql-tools");
-const users = [
-    {
-        id: 1,
-        name: 'john',
-        email: 'john@email.com'
-    },
-    {
-        id: 2,
-        name: 'Dany',
-        email: 'dany@email.com'
-    },
-    {
-        id: 3,
-        name: 'David',
-        email: 'david@email.com'
-    }
-];
-const typeDefs = `
-    type User {
-        id: ID!
-        name: String!
-        email: String!
-    }
-
-    type Query {
-        allUsers: [User!]!
-    }
-
-    type Mutation {
-        createUser(name: String!, email: String!): User
+const query_1 = require("./query");
+const mutation_1 = require("./mutation");
+const user_schema_1 = require("./resources/user/user.schema");
+const post_schema_1 = require("./resources/post/post.schema");
+const comment_schema_1 = require("./resources/comment/comment.schema");
+const SchemaDefinition = `
+    type Schema {
+        query: Query
+        mutation: Mutation
     }
 `;
-const resolvers = {
-    /*
-    Implementação de resolvers triviais
-    
-    User: {
-        id: (user) => user.id,
-        name: (user) => user.name,
-        email: (user) => user.email
-    }, */
-    Query: {
-        allUsers: () => users
-    },
-    Mutation: {
-        createUser: (parent, args) => {
-            const newUser = Object.assign({ id: users.length + 1 }, args);
-            users.push(newUser);
-            return newUser;
-        }
-    }
-};
-exports.default = graphql_tools_1.makeExecutableSchema({ typeDefs, resolvers });
+exports.default = graphql_tools_1.makeExecutableSchema({
+    typeDefs: [
+        SchemaDefinition,
+        query_1.Query,
+        mutation_1.Mutation,
+        user_schema_1.userTypes,
+        post_schema_1.postTypes,
+        comment_schema_1.commentTypes
+    ]
+});
